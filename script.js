0bb277efe8781ad49242e8b4071aef66ec765d4c
ro_g <97912154+Ro-Galvan@users.noreply.github.com>
2022-03-04 20:22:16 -0500

    saving work

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-console.log('does this work')
 // setting date to autopopulate 
 $('#currentDay').text(moment().format('dddd, MMMM Do, YYYY'));
+var saveBtn = document.querySelectorAll(".saveBtn");
+
+function changesColors(){
+    var time = moment().hour;
+    console.log(time);
+}
+
+// $("col-1 hour").text(time);
+
+// $('.time-block').each(function() {
+//     if (hour < time) {
+//         color =
+//     } else {
+        
+//     }
+// }
