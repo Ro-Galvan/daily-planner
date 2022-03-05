a5fadee334333e91c7f332b27c0f0b8892c6c737
ro_g <97912154+Ro-Galvan@users.noreply.github.com>
2022-03-05 10:52:03 -0500

    added if, else if, else so the colors could change and local strorage

--- script.js
+++ script.js
@@ -1,18 +1,43 @@
 // setting date to autopopulate 
 $('#currentDay').text(moment().format('dddd, MMMM Do, YYYY'));
-var saveBtn = document.querySelectorAll(".saveBtn");
+
+document.querySelectorAll('.row').forEach(row => {
+    const hourElement = row.querySelector('.hour');
+    const descriptionElement = row.querySelector('.description');
+    const saveBtn = row.querySelector(".saveBtn");
+    const hour = hourElement.textContent;
+    const date = new Date();
+    const hourAmp = hour.split(" ");
+    const currentHour = date.getHours();
+
+    //split array to isolate AM/PM and add 12 hours for military 
+    let hourInNumber = parseInt(hourAmp[0], 10);
+    if (hourAmp[1] === 'PM' && hourInNumber !== 12) {
+        hourInNumber += 12;
+    }
+    const text = localStorage.getItem(hourInNumber.toString());
+    
+    if (text) {
+        descriptionElement.value = text;
+    }
+
+    // to change colors based on current hour
+    if (hourInNumber === currentHour) {
+        descriptionElement.classList.add('present')
+    } else if (hourInNumber > currentHour) {
+        descriptionElement.classList.add('future');
+    } else {
+        descriptionElement.classList.add('past');
+    }
+
+    saveBtn.addEventListener('click', () => {
+        if (!descriptionElement.value) return;
+        localStorage.setItem(hourInNumber.toString(), descriptionElement.value);
+    })
+})
 
 function changesColors(){
     var time = moment().hour;
     console.log(time);
 }
 
-// $("col-1 hour").text(time);
-
-// $('.time-block').each(function() {
-//     if (hour < time) {
-//         color =
-//     } else {
-        
-//     }
-// }
